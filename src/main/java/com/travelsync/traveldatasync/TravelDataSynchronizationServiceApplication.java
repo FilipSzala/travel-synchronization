44b92e0b4dc9554fcca29da06924ce454fd0ea89
filip <filip.szala96@gmail.com>
2025-03-20 10:48:38 +0100

    structure of project

---
+++ src/main/java/com/travelsync/traveldatasync/TravelDataSynchronizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travelsync.traveldatasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDataSynchronizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDataSynchronizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelsync/traveldatasync/TravelDataSynchronizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelsync.traveldatasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDataSynchronizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
